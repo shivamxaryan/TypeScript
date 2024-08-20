089261517eecb9358fba7738f02765eac275982b
shivamxaryan <shivamaryan64@gmail.com>
2024-08-21 01:39:31 +0530

    fix: formatted

--- 02Basics/functions.ts
+++ 02Basics/functions.ts
@@ -1,20 +1,20 @@
-function addTwo (num: number) {
-    // num.toUpperCase();   wont able to use as it is a number
-    return num + 2;  // return "hello"  this will be allowed here
+function addTwo(num: number) {
+  // num.toUpperCase();   wont able to use as it is a number
+  return num + 2; // return "hello"  this will be allowed here
 }
 
-function addTwo1 (num: number): number {
-    return num + 2;
-    // return "hello"; this is not allowed now
+function addTwo1(num: number): number {
+  return num + 2;
+  // return "hello"; this is not allowed now
 }
 
 function upperCase(str: string) {
-    return str.toUpperCase();
+  return str.toUpperCase();
 }
 
 let loginUser = (isPaid: boolean = false) => {
-    return isPaid ? "paid" : "not paid";
-}
+  return isPaid ? "paid" : "not paid";
+};
 
 addTwo(5);
 addTwo1(5);
@@ -24,15 +24,15 @@ loginUser(true);
 const heroes = ["Batman", "Superman", "Spiderman"];
 
 heroes.map((hero): string => {
-    return `hero is ${hero}`;
-})
+  return `hero is ${hero}`;
+});
 
 function errMsg(err: string): void {
-    console.log(err);
+  console.log(err);
 }
 
-function errMessage (msg: string): never {
-    throw new Error(msg);  // used never because it will never return anything
+function errMessage(msg: string): never {
+  throw new Error(msg); // used never because it will never return anything
 }
 
 export {};
